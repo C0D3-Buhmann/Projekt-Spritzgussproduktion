89c54e14ede2d59af80c4da6f0a2d1134be369e1
moritz.rueben <moritz.rueben@t-online.de>
2022-11-09 14:50:57 +0100

    Der Benötigte rest

---
+++ LukasBibliothek/KundenKontaktAnzeige.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace LukasBibliothek
+{
+    partial class KundenKontaktAnzeige
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btnSp = new System.Windows.Forms.Button();
+            this.nudKuID = new System.Windows.Forms.NumericUpDown();
+            this.lblK = new System.Windows.Forms.Label();
+            this.nudKoID = new System.Windows.Forms.NumericUpDown();
+            this.lblKKID = new System.Windows.Forms.Label();
+            this.cBoxK = new System.Windows.Forms.ComboBox();
+            this.lblKk = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl = new System.Windows.Forms.Label();
+            this.lblKoE = new System.Windows.Forms.Label();
+            this.lblAI = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.nubAID = new System.Windows.Forms.NumericUpDown();
+            this.lblAID = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudKuID)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudKoID)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nubAID)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Red;
+            this.btnExit.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnExit.Location = new System.Drawing.Point(698, 408);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(90, 30);
+            this.btnExit.TabIndex = 20;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnB
+            // 
+            this.btnB.Location = new System.Drawing.Point(698, 48);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(90, 30);
+            this.btnB.TabIndex = 19;
+            this.btnB.Text = "Bearbeiten";
+            this.btnB.UseVisualStyleBackColor = true;
+            // 
+            // btnSp
+            // 
+            this.btnSp.Location = new System.Drawing.Point(698, 12);
+            this.btnSp.Name = "btnSp";
+            this.btnSp.Size = new System.Drawing.Size(90, 30);
+            this.btnSp.TabIndex = 18;
+            this.btnSp.Text = "Speichern";
+            this.btnSp.UseVisualStyleBackColor = true;
+            // 
+            // nudKuID
+            // 
+            this.nudKuID.Location = new System.Drawing.Point(156, 110);
+            this.nudKuID.Name = "nudKuID";
+            this.nudKuID.Size = new System.Drawing.Size(80, 26);
+            this.nudKuID.TabIndex = 28;
+            // 
+            // lblK
+            // 
+            this.lblK.AutoSize = true;
+            this.lblK.Location = new System.Drawing.Point(15, 116);
+            this.lblK.Name = "lblK";
+            this.lblK.Size = new System.Drawing.Size(85, 20);
+            this.lblK.TabIndex = 27;
+            this.lblK.Text = "Kunden ID";
+            // 
+            // nudKoID
+            // 
+            this.nudKoID.Location = new System.Drawing.Point(156, 60);
+            this.nudKoID.Name = "nudKoID";
+            this.nudKoID.Size = new System.Drawing.Size(80, 26);
+            this.nudKoID.TabIndex = 26;
+            // 
+            // lblKKID
+            // 
+            this.lblKKID.AutoSize = true;
+            this.lblKKID.Location = new System.Drawing.Point(15, 66);
+            this.lblKKID.Name = "lblKKID";
+            this.lblKKID.Size = new System.Drawing.Size(85, 20);
+            this.lblKKID.TabIndex = 25;
+            this.lblKKID.Text = "Kontakt ID";
+            // 
+            // cBoxK
+            // 
+            this.cBoxK.FormattingEnabled = true;
+            this.cBoxK.Location = new System.Drawing.Point(156, 19);
+            this.cBoxK.Name = "cBoxK";
+            this.cBoxK.Size = new System.Drawing.Size(160, 28);
+            this.cBoxK.TabIndex = 24;
+            // 
+            // lblKk
+            // 
+            this.lblKk.AutoSize = true;
+            this.lblKk.Location = new System.Drawing.Point(15, 22);
+            this.lblKk.Name = "lblKk";
+            this.lblKk.Size = new System.Drawing.Size(62, 20);
+            this.lblKk.TabIndex = 23;
+            this.lblKk.Text = "kontakt";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(428, 103);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 30;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(428, 145);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
+            this.textBox4.TabIndex = 32;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(428, 185);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 26);
+            this.textBox5.TabIndex = 33;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(428, 225);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 26);
+            this.textBox6.TabIndex = 34;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(273, 111);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(96, 20);
+            this.lblInfo.TabIndex = 36;
+            this.lblInfo.Text = "Kontakt Info";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(273, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 20);
+            this.label4.TabIndex = 38;
+            this.label4.Text = "Kunden Tel.";
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(273, 191);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(94, 20);
+            this.lbl.TabIndex = 39;
+            this.lbl.Text = "Kontakt Fax";
+            // 
+            // lblKoE
+            // 
+            this.lblKoE.AutoSize = true;
+            this.lblKoE.Location = new System.Drawing.Point(273, 231);
+            this.lblKoE.Name = "lblKoE";
+            this.lblKoE.Size = new System.Drawing.Size(112, 20);
+            this.lblKoE.TabIndex = 40;
+            this.lblKoE.Text = "Kontakt E-mail";
+            // 
+            // lblAI
+            // 
+            this.lblAI.AutoSize = true;
+            this.lblAI.Location = new System.Drawing.Point(273, 271);
+            this.lblAI.Name = "lblAI";
+            this.lblAI.Size = new System.Drawing.Size(149, 20);
+            this.lblAI.TabIndex = 41;
+            this.lblAI.Text = "Ansprechpartner ID";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(428, 265);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 26);
+            this.textBox7.TabIndex = 42;
+            // 
+            // nubAID
+            // 
+            this.nubAID.Location = new System.Drawing.Point(156, 170);
+            this.nubAID.Name = "nubAID";
+            this.nubAID.Size = new System.Drawing.Size(80, 26);
+            this.nubAID.TabIndex = 44;
+            // 
+            // lblAID
+            // 
+            this.lblAID.AutoSize = true;
+            this.lblAID.Location = new System.Drawing.Point(15, 176);
+            this.lblAID.Name = "lblAID";
+            this.lblAID.Size = new System.Drawing.Size(91, 20);
+            this.lblAID.TabIndex = 43;
+            this.lblAID.Text = "Auftrags ID";
+            // 
+            // KundenKontaktAnzeige
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nubAID);
+            this.Controls.Add(this.lblAID);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.lblAI);
+            this.Controls.Add(this.lblKoE);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.nudKuID);
+            this.Controls.Add(this.lblK);
+            this.Controls.Add(this.nudKoID);
+            this.Controls.Add(this.lblKKID);
+            this.Controls.Add(this.cBoxK);
+            this.Controls.Add(this.lblKk);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnSp);
+            this.Name = "KundenKontaktAnzeige";
+            this.Text = "KundenKontaktAnzeige";
+            ((System.ComponentModel.ISupportInitialize)(this.nudKuID)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudKoID)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nubAID)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Button btnSp;
+        private System.Windows.Forms.NumericUpDown nudKuID;
+        private System.Windows.Forms.Label lblK;
+        private System.Windows.Forms.NumericUpDown nudKoID;
+        private System.Windows.Forms.Label lblKKID;
+        private System.Windows.Forms.ComboBox cBoxK;
+        private System.Windows.Forms.Label lblKk;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label lblKoE;
+        private System.Windows.Forms.Label lblAI;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.NumericUpDown nubAID;
+        private System.Windows.Forms.Label lblAID;
+    }
+}
---
+++ LukasBibliothek/KundenKontaktAnzeige.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LukasBibliothek
+{
+    public partial class KundenKontaktAnzeige : Form
+    {
+        public KundenKontaktAnzeige()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
