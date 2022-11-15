7aa1a2c64ca480ec985a801f0d549e5a102a0eea
moritz.rueben <moritz.rueben@t-online.de>
2022-11-15 12:56:57 +0100

    hjk

---
+++ Projekt-Spritzgussproduktion/test.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Projekt_Spritzgussproduktion
+{
+    public partial class test : UserControl
+    {
+        public test()
+        {
+            InitializeComponent();
+        }
+    }
+}
